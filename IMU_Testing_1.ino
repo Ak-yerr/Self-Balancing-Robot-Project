88fefe381aa49a59a45a423d493e80cc7197ed4d
Akhil Yerrapally <akhily2@illinois.edu>
2026-06-14 00:14:40 -0500

    Update IMU_Testing_1.ino

--- IMU_Testing_1.ino
+++ IMU_Testing_1.ino
@@ -4,11 +4,11 @@ void setup() {
   Serial.begin(115200);
   Wire.begin(21, 22);
   Serial.println("Scanning...");
-
+// begin scanning for whether the device is found. P21 -> SDA, P22 -> SCL
   for (byte address = 1; address < 127; address++) {
     Wire.beginTransmission(address);
     byte error = Wire.endTransmission();
-
+// loop to find the device at address
     if (error == 0) {
       Serial.print("Device found at 0x");
       Serial.println(address, HEX);
